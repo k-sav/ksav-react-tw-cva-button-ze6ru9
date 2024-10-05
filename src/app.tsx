16cf8065e4c20f6e3a9e426c820bf8f12840d666
Kris <kris@linktr.ee>
2024-10-05 14:27:53 +1000

    update

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
-import { cx } from 'class-variance-authority';
-import { Button } from './components';
+import { cx } from "class-variance-authority";
+import { Button } from "./components";
 
-const intents = [undefined, 'primary', 'secondary', 'tertiary'] as const;
-const sizes = [undefined, 'medium', 'small'] as const;
+const intents = [undefined, "primary", "secondary", "tertiary"] as const;
+const sizes = [undefined, "medium", "small"] as const;
 
-const radius = '3xl';
+const radius = "3xl";
 
 const Icon = ({ className }: { className?: string }) => (
   <svg viewBox="0 0 14 16" className={className}>
@@ -20,24 +20,24 @@ function App() {
     <>
       <table
         className={cx(
-          'relative',
-          'h-max w-max',
-          'self-center justify-self-center',
-          '[&_:where(th,td)]:p-2'
+          "relative",
+          "h-max w-max",
+          "self-center justify-self-center",
+          "[&_:where(th,td)]:p-2"
         )}
       >
         <thead>
           <tr>
             <th></th>
             {intents.map((intent) => (
-              <th scope="col">{intent || 'default'}</th>
+              <th scope="col">{intent || "default"}</th>
             ))}
           </tr>
         </thead>
         <tbody>
           {sizes.map((size) => (
             <tr>
-              <th scope="row">{size || 'default'}</th>
+              <th scope="row">{size || "default"}</th>
               {intents.map((intent) => (
                 <td scope="col">
                   <Button
@@ -45,7 +45,7 @@ function App() {
                     {...(intent && { intent })}
                     {...(size && { size })}
                   >
-                    {intent || 'default'} button
+                    {intent || "default"} button
                   </Button>
                 </td>
               ))}
@@ -100,7 +100,9 @@ function App() {
           </Button>
         </div>
 
-        <button className="focusable">Super custom unicorn button</button>
+        <button className="focusable self-center underline">
+          Super custom unicorn button 🦄
+        </button>
       </div>
     </>
   );
